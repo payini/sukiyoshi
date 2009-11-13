476d3652fb1a5fb882a53a090d6546baa0a67f68
stiles.mark <stiles.mark@fdcffc38-77cd-11de-b8b8-6d0436ffb0fe>
2009-11-13 13:50:56 +0000

    Sitecore Integration Support

---
+++ trunk/OTP.Web.BrightcoveAPI.SitecoreUtil/BCAccount.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Sitecore.Data.Items;
+using Sitecore.Data;
+using OTP.Web.BrightcoveAPI.SitecoreUtil.Extensions;
+
+namespace OTP.Web.BrightcoveAPI.SitecoreUtil
+{
+	public class BCAccount : Item {
+
+		public BCAccount(ID itemID, ItemData data, Database database) : base(itemID, data, database) {
+
+		}
+
+		public long PublisherID {
+			get {
+				if (this.Fields["Publisher ID"].Value.Equals("")) {
+					return 0;
+				}
+				else {
+					return long.Parse(this.Fields["Publisher ID"].Value);
+				}
+			}
+			set {
+				this.Fields["Publisher ID"].Value = value.ToString();
+			}
+		}
+
+		public PlaylistLibrary PlaylistLib {
+			get {
+				return new PlaylistLibrary(this.ChildByTemplate("Playlist Library"));
+			}
+		}
+		
+		public VideoLibrary VideoLib {
+			get {
+				return new VideoLibrary(this.ChildByTemplate("Video Library"));
+			}
+		}
+
+		public VideoPlayerLibrary PlayerLib {
+			get {
+				return new VideoPlayerLibrary(this.ChildByTemplate("Video Player Library"));
+			}
+		}
+	}
+}
