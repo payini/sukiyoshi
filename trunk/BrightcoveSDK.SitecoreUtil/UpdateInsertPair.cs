43f9fa0361c0923b578d25515c49e8e258bfb8b1
stiles.mark <stiles.mark@fdcffc38-77cd-11de-b8b8-6d0436ffb0fe>
2009-12-29 22:19:00 +0000

    changed the namespace

---
+++ trunk/BrightcoveSDK.SitecoreUtil/UpdateInsertPair.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BrightcoveAPI.Media;
+using Sitecore.Data.Items;
+
+namespace BrightcoveAPI.SitecoreUtil
+{
+	public class UpdateInsertPair<T>
+	{
+		public List<T> NewItems = new List<T>();
+		public List<T> UpdatedItems = new List<T>();
+	}
+}
